ea9fb3666e937f6cfb941e80985bb74664f8b4ec
Naoya-Oda-sti <n-oda@sios.com>
2024-10-09 17:29:32 +0900

    chat_models.pyを修正

--- chat_models.py
+++ chat_models.py
@@ -1,13 +1,16 @@
-# from langchain.chat_models import ChatOpenAI azure or openai
+# AzureOpenAIではなくAzureChatOpenAIを使う
+# https://stackoverflow.com/questions/78656632/the-completion-operation-does-not-work-with-the-specified-model-gpt-4
 from langchain_openai import AzureChatOpenAI
-# https://python.langchain.com/v0.2/docs/integrations/chat/azure_chat_openai/
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
+import os
 
-chat = AzureChatOpenAI(temperature=0)
-output = chat.predict("日本の総理大臣は誰ですか？") 
+chat = AzureChatOpenAI(
+    azure_endpoint=os.getenv("AOAI_ENDPOINT"),
+    api_key=os.getenv("AOAI_API_KEY"),
+    api_version=os.getenv("AOAI_API_VERSION"),
+    openai_api_type="azure",
+    azure_deployment=os.getenv("AOAI_DEPLOYMENT"),
+    temperature=0
+)
+output = chat.predict("私の名前は何でしょうか？") 
 
 print(output)
